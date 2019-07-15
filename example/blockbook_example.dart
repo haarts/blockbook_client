267289c890395037008c4a6e7fa2c0998e163785
Harm Aarts <harm@aarts.email>
2019-07-15 14:38:42 +0200

    fmt

--- example/blockbook_example.dart
+++ example/blockbook_example.dart
@@ -4,9 +4,9 @@ main(List<String> args) async {
   var client = Blockbook("https://btc1.trezor.io", "wss://btc1.trezor.io/websocket");
   print('status:\n ${await client.status()}');
 
-	Stream txs = client.subscribeAddresses([args[0]]);
-	txs = txs.handleError(print);
+  Stream txs = client.subscribeAddresses([args[0]]);
+  txs = txs.handleError(print);
   await for (var tx in txs) {
-   print(tx);
+    print(tx);
   };
 }
