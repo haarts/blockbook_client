14f3f0963026fb7166d41eff1625748cc3fb27fe
Dominik Spicher <dominik.spicher@inacta.ch>
2019-10-25 16:09:33 +0200

    Format

--- example/blockbook_example.dart
+++ example/blockbook_example.dart
@@ -1,12 +1,13 @@
 import 'package:blockbook/blockbook.dart';
 
 main(List<String> args) async {
-  var client = Blockbook("https://btc1.trezor.io", "wss://btc1.trezor.io/websocket");
+  var client =
+      Blockbook("https://btc1.trezor.io", "wss://btc1.trezor.io/websocket");
   print('status:\n ${await client.status()}');
 
   Stream txs = client.subscribeAddresses([args[0]]);
   txs = txs.handleError(print);
   await for (var tx in txs) {
     print(tx);
-  };
+  }
 }
