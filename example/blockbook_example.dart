d4946de5aa59196da72f62be5a05a0a6289f8489
Harm Aarts <harm@aarts.email>
2020-01-03 14:25:03 +0100

    Linter nits

--- example/blockbook_example.dart
+++ example/blockbook_example.dart
@@ -1,11 +1,11 @@
 import 'package:blockbook/blockbook.dart';
 
-main(List<String> args) async {
+void main(List<String> args) async {
   var client =
-      Blockbook("https://btc1.trezor.io", "wss://btc1.trezor.io/websocket");
+      Blockbook('https://btc1.trezor.io', 'wss://btc1.trezor.io/websocket');
   print('status:\n ${await client.status()}');
 
-  Stream txs = client.subscribeAddresses([args[0]]);
+  var txs = client.subscribeAddresses([args[0]]);
   txs = txs.handleError(print);
   await for (var tx in txs) {
     print(tx);
--- test/blockbook_test.dart
+++ test/blockbook_test.dart
@@ -12,14 +12,14 @@ void main() {
   setUp(() async {
     server = MockWebServer();
     await server.start();
-    blockbook = Blockbook(server.url, "ws://${server.host}:${server.port}/ws");
+    blockbook = Blockbook(server.url, 'ws://${server.host}:${server.port}/ws');
   });
 
   tearDown(() {
     server.shutdown();
   });
 
-  test("exported URLs", () {
+  test('exported URLs', () {
     var restUrl = 'http://foo.bar';
     var websocketUrl = 'ws://foo.bar';
 
@@ -29,138 +29,137 @@ void main() {
     expect(blockbook.websocketUrl, Uri.parse(websocketUrl));
   });
 
-  group("http API calls", () {
+  group('http API calls', () {
     test('status', () async {
-      server.enqueue(body: File("test/files/status.json").readAsStringSync());
+      server.enqueue(body: File('test/files/status.json').readAsStringSync());
 
       var status = await blockbook.status();
 
-      expect(status["blockbook"]["coin"], "Bitcoin");
+      expect(status['blockbook']['coin'], 'Bitcoin');
     });
 
     test('blockHash', () async {
       server.enqueue(
-          body: File("test/files/blockHash.json").readAsStringSync());
+          body: File('test/files/blockHash.json').readAsStringSync());
 
       var blockHash = await blockbook.blockHash(500000);
 
-      expect(server.takeRequest().uri.path, endsWith("500000"));
+      expect(server.takeRequest().uri.path, endsWith('500000'));
       expect(blockHash,
-          "00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04");
+          '00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04');
     });
 
     test('transaction', () async {
       server.enqueue(
-          body: File("test/files/transaction.json").readAsStringSync());
+          body: File('test/files/transaction.json').readAsStringSync());
 
       var tx = await blockbook.transaction(
-          "4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e");
+          '4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e');
 
       expect(
           server.takeRequest().uri.path,
           endsWith(
-              "4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e"));
-      expect(tx["txid"],
-          "4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e");
+              '4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e'));
+      expect(tx['txid'],
+          '4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e');
     });
 
     test('transactionSpecific', () async {
       server.enqueue(
-          body: File("test/files/transactionSpecific.json").readAsStringSync());
+          body: File('test/files/transactionSpecific.json').readAsStringSync());
 
       var tx = await blockbook.transactionSpecific(
-          "4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e");
+          '4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e');
 
       expect(
           server.takeRequest().uri.path,
           endsWith(
-              "tx-specific/4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e"));
-      expect(tx["txid"],
-          "4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e");
+              'tx-specific/4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e'));
+      expect(tx['txid'],
+          '4a4c48638ffd14fca86c663ce6bfb6edd7dbe8538284cd23bad5c21a498d086e');
     });
 
     test('address', () async {
-      server.enqueue(body: File("test/files/address.json").readAsStringSync());
+      server.enqueue(body: File('test/files/address.json').readAsStringSync());
 
       var address =
-          await blockbook.address("bc1qzq4dsuku95evf89sulryzkx7uasnkh4zwwt6q4");
+          await blockbook.address('bc1qzq4dsuku95evf89sulryzkx7uasnkh4zwwt6q4');
 
       expect(server.takeRequest().uri.path,
-          endsWith("bc1qzq4dsuku95evf89sulryzkx7uasnkh4zwwt6q4"));
-      expect(address["address"], "bc1qzq4dsuku95evf89sulryzkx7uasnkh4zwwt6q4");
+          endsWith('bc1qzq4dsuku95evf89sulryzkx7uasnkh4zwwt6q4'));
+      expect(address['address'], 'bc1qzq4dsuku95evf89sulryzkx7uasnkh4zwwt6q4');
     });
 
     test('xpub', () async {
-      server.enqueue(body: File("test/files/xpub.json").readAsStringSync());
+      server.enqueue(body: File('test/files/xpub.json').readAsStringSync());
 
       var xpub = await blockbook.xpub(
-          "xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz");
+          'xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz');
 
       expect(
           server.takeRequest().uri.path,
           endsWith(
-              "xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz"));
-      expect(xpub["address"],
-          "xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz");
+              'xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz'));
+      expect(xpub['address'],
+          'xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz');
     });
 
     test('utxo', () async {
-      server.enqueue(body: File("test/files/utxo.json").readAsStringSync());
+      server.enqueue(body: File('test/files/utxo.json').readAsStringSync());
 
-      var utxo = await blockbook.utxo("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa");
+      var utxo = await blockbook.utxo('1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa');
 
       expect(server.takeRequest().uri.path,
-          endsWith("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"));
-      expect(utxo[0]["txid"],
-          "7a0fc95fdc459bee2f5bc7156ff23dcc84b6dbaadf34d9d2fc79b01179c1a6bf");
+          endsWith('1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa'));
+      expect(utxo[0]['txid'],
+          '7a0fc95fdc459bee2f5bc7156ff23dcc84b6dbaadf34d9d2fc79b01179c1a6bf');
     });
 
     test('block (with hash)', () async {
-      server.enqueue(body: File("test/files/block.json").readAsStringSync());
+      server.enqueue(body: File('test/files/block.json').readAsStringSync());
 
       var block = await blockbook.block(
-          "00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04");
+          '00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04');
 
       expect(
           server.takeRequest().uri.path,
           endsWith(
-              "00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04"));
-      expect(block["hash"],
-          "00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04");
+              '00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04'));
+      expect(block['hash'],
+          '00000000000000000024fb37364cbf81fd49cc2d51c09c75c35433c3a1945d04');
     });
 
     test('block (with height)', () async {
-      server.enqueue(body: File("test/files/block.json").readAsStringSync());
+      server.enqueue(body: File('test/files/block.json').readAsStringSync());
 
       var block = await blockbook.block(500000);
 
-      expect(server.takeRequest().uri.path, endsWith("500000"));
-      expect(block["height"], 500000);
+      expect(server.takeRequest().uri.path, endsWith('500000'));
+      expect(block['height'], 500000);
     });
 
     test('sendTransaction', () {}, skip: 'todo');
   });
 
-  group("websocket calls", () {
+  group('websocket calls', () {
     test('getInfo', () async {
       server.enqueue(
-          body: File("test/files/ws_getInfo.json").readAsStringSync());
+          body: File('test/files/ws_getInfo.json').readAsStringSync());
 
-      Stream s = blockbook.getInfo();
-      s.listen(expectAsync1((message) {
+      blockbook.getInfo().listen(expectAsync1((message) {
         expect(message, isNotNull);
       }));
     });
 
-    test("getBlockHash", () async {}, skip: "todo");
-    test("getAccountInfo", () async {}, skip: "todo");
-    test("getAccountUtxo", () async {}, skip: "todo");
-    test("getTransaction", () async {}, skip: "todo");
-    test("getTransactionSpecific", () async {}, skip: "todo");
-    test("estimateFee", () async {}, skip: "todo");
-    test("sendTransaction", () async {}, skip: "todo");
+    test('getBlockHash', () async {}, skip: 'todo');
+    test('getAccountInfo', () async {}, skip: 'todo');
+    test('getAccountUtxo', () async {}, skip: 'todo');
+    test('getTransaction', () async {}, skip: 'todo');
+    test('getTransactionSpecific', () async {}, skip: 'todo');
+    test('estimateFee', () async {}, skip: 'todo');
+    test('sendTransaction', () async {}, skip: 'todo');
 
-    test("subscribeNewBlock", () async {
+    test('subscribeNewBlock', () async {
       var subscriptionSuccess = '{"id":"1","data":{"subscribed":true}}';
       var newBlock =
           '{"id":"1","data":{"height":611099,"hash":"00000000000000000010657f651f9a65814a3ba731ea997304ebcd6d9cf150eb"}}';
@@ -178,13 +177,13 @@ void main() {
       );
     });
 
-    test("subscribeAddresses", () async {
+    test('subscribeAddresses', () async {
       server.enqueue(
           body:
-              File("test/files/ws_subscribeAddresses.json").readAsStringSync());
+              File('test/files/ws_subscribeAddresses.json').readAsStringSync());
 
-      Stream s = blockbook.subscribeAddresses(["some-address"]);
-      s.listen(expectAsync1((message) {
+      blockbook
+          .subscribeAddresses(['some-address']).listen(expectAsync1((message) {
         expect(message, isNotNull);
       }));
     });
